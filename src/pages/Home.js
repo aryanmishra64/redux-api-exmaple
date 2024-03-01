d0ab57848b7d096719a3c41c42f94a6a3ed33de4
aryanmishra64 <aryanthechamp64@gmail.com>
2024-03-01 21:59:00 +0530

    integrated admin panel

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import NavBar from "../features/navbar/Navbar";
+import ProductList from "../features/product-list/components/ProductList";
+
+
+function Home () {
+  return (
+    <div>
+         <NavBar>
+            <ProductList></ProductList>
+         </NavBar>
+    </div>
+  );
+}
+
+export default Home;
